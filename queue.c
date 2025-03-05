58d748389c9e9cb6322c1704da6248a0dc9cbb97
MikeLiu <mike0976365088@gmail.com>
2025-03-05 16:28:15 +0800

    Implement free function
    Unlink all nodes in the queue, including the head node.
    After unlink the node, free the occupied memory.
    If there is no head node, terminate execution.
    
    Change-Id: I791db3329741c6a2b7966ceadf45d7352c5b4239

--- queue.c
+++ queue.c
@@ -18,7 +18,24 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    struct list_head *cur = head->next;
+
+    while (cur != head) {
+        struct list_head *tmp;
+        tmp = cur;
+        cur = cur->next;
+
+        list_del(tmp);
+        q_release_element(list_entry(tmp, element_t, list));
+    }
+
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
