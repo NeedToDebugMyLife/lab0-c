7faab0212f0fefe8f3dfb7bdfc27a5f3eebea997
MikeLiu <mike0976365088@gmail.com>
2025-03-05 16:47:03 +0800

    Implement remove tail node
    Unlink the last node of queue, then copy the value into stack pointer.
    If removal is successful, return the node.
    If there is no head node or element, return NULL.
    
    Change-Id: I9627f6bf54042dd54805ef02b714027cb3c1cba5

--- queue.c
+++ queue.c
@@ -113,7 +113,18 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *node = list_entry(head->prev, element_t, list);
+    char *str = strndup(node->value, bufsize - 1);
+
+    strcat(str, "\0");
+    strncpy(sp, str, bufsize);
+
+    list_del(&node->list);
+
+    return node;
 }
 
 /* Return number of elements in queue */
