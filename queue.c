15e54bd63478abc863dda2434954dcd5a086e1f5
MikeLiu <mike0976365088@gmail.com>
2025-03-06 13:15:17 +0800

    Implement reverse function
    Use loop to traverse each node.
    Start checking from the last node and
    move the checked node to the end of the queue.
    
    Change-Id: I258c2c10cac83a74af39924254d672f8efc747a6

--- queue.c
+++ queue.c
@@ -216,7 +216,20 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;
+
+    struct list_head *cur = head->prev;
+
+    while (cur != head) {
+        struct list_head *tmp;
+        tmp = cur;
+        cur = cur->prev;
+        list_move_tail(tmp, head);
+    }
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
